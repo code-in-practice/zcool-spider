f511706a9225f691929978f2fa6f8d2192ec6cbf
rocky <rocky@rockymac.com>
2015-10-16 22:04:09 +0800

    update parser

--- main.py
+++ main.py
@@ -5,15 +5,8 @@ url = 'http://www.zcool.com.cn/u/1'
 r = requests.get(url)
 bodydom = r.content
 # print(bodydom)
-bodysoup = BeautifulSoup(bodydom, 'lxml')
-namedom = bodysoup.find('div', class_='vm ubcTitle')
+bodysoup = BeautifulSoup(bodydom)
+namedom = bodysoup.find('div', class_='vm ubcTitle').text.strip()
 print(namedom)
-# namesoup = BeautifulSoup(namedom)
-# name = namesoup.get_text()
-# print(name)
-
-
-
-renqidom = bodysoup.find(attrs={'class': 'f20 cf90'}).string
-print('-----------------------------------------------------------')
+renqidom = bodysoup.find(attrs={'class': 'f20 cf90'}).text.strip()
 print(renqidom)
