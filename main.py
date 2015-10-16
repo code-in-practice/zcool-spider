f4d8fd082abb467423015bda5846892961afbca6
zhentou <zhangshijun.go@gmail.com>
2015-10-16 23:31:32 +0800

    update parser, finished the main page parse

--- main.py
+++ main.py
@@ -5,8 +5,27 @@ url = 'http://www.zcool.com.cn/u/1'
 r = requests.get(url)
 bodydom = r.content
 # print(bodydom)
-bodysoup = BeautifulSoup(bodydom)
-namedom = bodysoup.find('div', class_='vm ubcTitle').text.strip()
-print(namedom)
-renqidom = bodysoup.find(attrs={'class': 'f20 cf90'}).text.strip()
-print(renqidom)
+bodysoup = BeautifulSoup(bodydom, 'html.parser')
+username = bodysoup.find(attrs= {'class': 'ubcTitle'}).span.text.strip()
+print('username: ' + username)
+
+city = bodysoup.find(attrs= {'class': 'ubcTitle'}).next_sibling.next_sibling.text.strip().split('/')[0].strip()
+print('city: ' + city)
+
+profession = bodysoup.find(attrs= {'class': 'ubcTitle'}).next_sibling.next_sibling.text.strip().split('/')[1].strip()
+print('profession: ' + profession)
+
+signature = bodysoup.find(attrs= {'class': 'nanBox'}).p.text.strip()
+print('signature: ' + signature)
+
+popularity = bodysoup.findAll(attrs={'class': 'f20 cf90'})[0].text.strip()
+print('popularity: ' + popularity)
+
+score = bodysoup.findAll(attrs={'class': 'f20 cf90'})[1].text.strip()
+print('score: ' + score)
+
+follower_number = bodysoup.findAll(attrs={'class': 'f20 cf90'})[2].text.strip()
+print('follower_number: ' + follower_number)
+
+follow_number = bodysoup.findAll(attrs={'class': 'f20 cf90'})[3].text.strip()
+print('follow_number: ' + follow_number)
