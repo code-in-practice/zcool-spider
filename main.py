2c7a3c600dfbecac0da7a490c4748d129e032395
zhentou <zhangshijun.go@gmail.com>
2015-10-17 12:36:10 +0800

    refector the request code: merge ajax.py to main.py

--- main.py
+++ main.py
@@ -1,31 +1,84 @@
+#coding=utf-8
 import requests
 from bs4 import BeautifulSoup
+import json
 
-url = 'http://www.zcool.com.cn/u/1'
-r = requests.get(url)
-bodydom = r.content
-# print(bodydom)
-bodysoup = BeautifulSoup(bodydom, 'html.parser')
-username = bodysoup.find(attrs= {'class': 'ubcTitle'}).span.text.strip()
-print('username: ' + username)
 
-city = bodysoup.find(attrs= {'class': 'ubcTitle'}).next_sibling.next_sibling.text.strip().split('/')[0].strip()
-print('city: ' + city)
+def main_user(user_id):
+    url = 'http://www.zcool.com.cn/u/1'
+    r = requests.get(url)
+    bodydom = r.content
 
-profession = bodysoup.find(attrs= {'class': 'ubcTitle'}).next_sibling.next_sibling.text.strip().split('/')[1].strip()
-print('profession: ' + profession)
+    bodysoup = BeautifulSoup(bodydom, 'html.parser')
 
-signature = bodysoup.find(attrs= {'class': 'nanBox'}).p.text.strip()
-print('signature: ' + signature)
+    i=0
+    keys = ['popularity', 'score', 'follower_number', 'follow_number']
+    user_detail_dict = {}
 
-popularity = bodysoup.findAll(attrs={'class': 'f20 cf90'})[0].text.strip()
-print('popularity: ' + popularity)
+    username = bodysoup.find(attrs= {'class': 'ubcTitle'}).span.text.strip()
+    city = bodysoup.find(attrs= {'class': 'ubcTitle'}).next_sibling.next_sibling.text.strip().split('/')[0].strip()
+    profession = bodysoup.find(attrs= {'class': 'ubcTitle'}).next_sibling.next_sibling.text.strip().split('/')[1].strip()
+    signature = bodysoup.find(attrs= {'class': 'nanBox'}).p.text.strip()
 
-score = bodysoup.findAll(attrs={'class': 'f20 cf90'})[1].text.strip()
-print('score: ' + score)
+    user_detail_dict.update({'username': username})
+    user_detail_dict.update({'city': city})
+    user_detail_dict.update({'profession': profession})
+    user_detail_dict.update({'signature': signature})
 
-follower_number = bodysoup.findAll(attrs={'class': 'f20 cf90'})[2].text.strip()
-print('follower_number: ' + follower_number)
+    for w in bodysoup.find_all(attrs={'class': 'f20 cf90'}):
+        user_detail_dict.update({keys[i]: w.text.strip()})
+        i = i+1
 
-follow_number = bodysoup.findAll(attrs={'class': 'f20 cf90'})[3].text.strip()
-print('follow_number: ' + follow_number)
+    return user_detail_dict
+
+def ajax_user(user_id):
+    ajax_param = {'id': user_id}
+    ajax_url = 'http://www.zcool.com.cn/getPersonDetailInfo.do?jsonpCallback=?'
+    r = requests.post(ajax_url, data=ajax_param)
+    bodydom = r.content
+    bodysoup = BeautifulSoup(bodydom, 'html.parser')
+
+    i=0
+    keys = ['realname', 'hometown', 'mobile', 'last_login', 'birthday', 'address_city', 'qq', 'zcool_age', 'college', 'email', 'wechat', 'equipments', 'tags', 'description', 'links']
+    user_detail_dict = {}
+    user_detail_dict.update({'id': user_id})
+
+    # check if the user is available
+    status = r.status_code == 200
+    print(status)
+
+    if status:
+        for w in bodysoup.find_all('td'):
+            user_detail_dict.update({keys[i]: w.text.strip().split(u'：')[1].replace('\t', '')})
+            i = i+1
+        return user_detail_dict
+    else:
+        return False
+
+def zcool_user_info(user_id):
+
+    user_detail_ajax_dict = ajax_user(user_id)
+
+    if user_detail_ajax_dict:
+        user_detail_ajax_json = json.dumps(user_detail_ajax_dict, indent=4,ensure_ascii=False).encode('utf8')
+        user_detail_main_dict = main_user(user_id)
+        user_detail_mail_json = json.dumps(user_detail_main_dict, indent=4,ensure_ascii=False).encode('utf8')
+        user_detail_dict = user_detail_main_dict.copy()
+        user_detail_dict.update(user_detail_ajax_dict)
+        return user_detail_dict
+    else:
+        return False
+
+
+
+for i in xrange(1, 10000):
+    user_id = i
+    user_detail_dict = zcool_user_info(user_id)
+    if user_detail_dict:
+        user_detail_json = json.dumps(user_detail_dict, indent=4,ensure_ascii=False).encode('utf8')
+        print(user_detail_json)
+    else:
+        print('user: ' + str(user_id) + ' does not exist ------------------------------------')
+
+
+
