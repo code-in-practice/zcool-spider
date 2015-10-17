a3e574e4ff5f78b0fc2c96df679b25c683750a0d
zhentou <zhangshijun.go@gmail.com>
2015-10-17 10:18:19 +0800

    added mongo db connection related file

---
+++ db/mongo.py
@@ -0,0 +1,16 @@
+__author__ = 'smile'
+
+from pymongo import MongoClient
+import datetime
+
+db_url = '127.0.0.1'
+db_port = 27017
+client = MongoClient(db_url, db_port)
+
+db = client['zcool']
+collection = db['c']
+
+user = {"author": "Mike"}
+print(user)
+user_id = collection.insert_one(user).inserted_id
+print(user_id)
