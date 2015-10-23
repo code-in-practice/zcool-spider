dbcde46d9dd70c2311bb49e0b5d129d2c3358511
zhentou <zhangshijun.go@gmail.com>
2015-10-23 23:29:44 +0800

    added zcooler_recommend.py

---
+++ ar.py
@@ -0,0 +1,10 @@
+__author__ = 'smile'
+
+ar =[]
+
+def f(ar):
+    for i in xrange(1, 100):
+        ar.append(i)
+
+f(ar)
+print(ar)
--- zcooler_recommend.py
+++ zcooler_recommend.py
@@ -5,14 +5,16 @@ from bs4 import BeautifulSoup
 
 import json
 
-def main_user(user_id, p):
+
+def main_user(recomend_list, user_id, p):
     url = 'http://www.zcool.com.cn/u/' + str(user_id) + '/zcooler_recommend.xhtml?p=' + str(p)
     r = requests.get(url)
     bodysoup = BeautifulSoup(r.content, 'html.parser')
 
     if(bodysoup.find_all(attrs={'class': 'camLiCon'}).__len__() > 0):
         for w in bodysoup.find_all(attrs={'class': 'camLiCon'}):
-            print w.previous_sibling.previous_sibling['href']
+            # print w.previous_sibling.previous_sibling['href']
+            recomend_list.append(w.previous_sibling.previous_sibling['href'])
 
     page_this = bodysoup.find(attrs= {'class': 'selected', 'btnmode': 'true'}).text.strip()
     print 'user_id: ' + str(user_id) + ' page_this: ' + str(page_this)
@@ -20,8 +22,12 @@ def main_user(user_id, p):
     print 'user_id: ' + str(user_id) + ' page_total: ' + str(page_total)
     page_next = int(page_this) + 1
     if not page_next > int(page_total):
-        main_user(user_id, page_next)
+        main_user(recomend_list, user_id, page_next)
 
+
+recomend_list = []
 
 for i in xrange(1, 1000):
-    main_user(i, 1)
+    main_user(recomend_list, i, 1)
+    print recomend_list
+    recomend_list = []
